0b99685993a12371aabcd9c3a9ffe854d7b173ce
Nhitro <julien53laval@gmail.com>
2020-01-16 13:16:34 +0100

    log des animations d'entrée/sortie

--- app/src/main/java/com/jgarnier/test/FragmentA.java
+++ app/src/main/java/com/jgarnier/test/FragmentA.java
@@ -4,13 +4,19 @@ package com.jgarnier.test;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class FragmentA extends Fragment {
+
+    private static final String TAG = FragmentA.class.getName();
 
     public FragmentA() {
         // Required empty public constructor
@@ -27,4 +33,22 @@ public class FragmentA extends Fragment {
         return inflater.inflate(R.layout.fragment_a, container, false);
     }
 
+    @Nullable
+    @Override
+    public Animation onCreateAnimation(int transit, boolean enter, int nextAnim) {
+        Animation animation= null;
+
+        if (nextAnim != 0) {
+            animation = AnimationUtils.loadAnimation(getContext(), nextAnim);
+            String name = getResources().getResourceName(nextAnim);
+            if (enter) {
+                Log.d(TAG, String.format("popEnter avec l'animation : %s", name));
+            } else {
+                Log.d(TAG, String.format("exit avec l'animation : %s", name));
+            }
+
+        }
+
+        return animation;
+    }
 }
--- app/src/main/java/com/jgarnier/test/FragmentB.java
+++ app/src/main/java/com/jgarnier/test/FragmentB.java
@@ -3,15 +3,21 @@ package com.jgarnier.test;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 
 public class FragmentB extends Fragment {
+
+    private static final String TAG = FragmentB.class.getName();
 
     public FragmentB() {
         // Required empty public constructor
@@ -25,5 +31,24 @@ public class FragmentB extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_b, container, false);
+    }
+
+    @Nullable
+    @Override
+    public Animation onCreateAnimation(int transit, boolean enter, int nextAnim) {
+        Animation animation= null;
+
+        if (nextAnim != 0) {
+            animation = AnimationUtils.loadAnimation(getContext(), nextAnim);
+            String name = getResources().getResourceName(nextAnim);
+            if (enter) {
+                Log.d(TAG, String.format("enter avec l'animation : %s", name));
+            } else {
+                Log.d(TAG, String.format("popExit avec l'animation : %s", name));
+            }
+
+        }
+
+        return animation;
     }
 }
